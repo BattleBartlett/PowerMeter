5133f1df875b4f5df2d1b85d90e1356c2b7e719c
BattleBartlett <36284171+BattleBartlett@users.noreply.github.com>
2018-06-12 08:04:01 -0230

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -17,6 +17,31 @@ done
 
 echo "Watchdog file($file) has not been updated within $timeout minutes - Rebooting"
 
+#send email start
+
+TO="chris.bartlett@live.ca"
+SUBJECT="PowerMeter Reboot"
+BODY="Watchdog file($file) has not been updated within $timeout minutes - Rebooting"
+
+# Add padding to email message
+#echo -e "\n\n" > "$BODY"
+#echo "$MSG" >> "$BODY"
+#/echo -e "\r\n" >> "$BODY"
+
+if [ -f "$BODY" ]
+then
+   # Send mail if message exists
+   mail -s "$SUBJECT" "$TO" < "$BODY"
+else
+   echo "Sending mail failed, please try again."
+   exit
+fi
+
+# Delete sent message
+rm -f "$BODY"
+
+#send email end
+
 curl -X POST --header "Content-Type:application/json" \
     "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
 
